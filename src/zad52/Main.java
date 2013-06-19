aa691951a06c82477ec58c084bc6634c6799237f
Michal Rogozinski <mr@mr-servers.net>
2013-06-18 22:20:33 -0700

    import Zad Z52

--- src/zad52/Main.java
+++ src/zad52/Main.java
@@ -1,13 +1,22 @@
 /**
  *
- *  @author Rogozi?ski Micha? S10324
+ *  @author Rogoziński Michał S10324
  *
  */
 package zad52;
 
+import javax.swing.JFrame;
 
 public class Main {
   public static void main(String ... args) {
   
+	  // utworzenie instancji okna głównego aplikacji
+	  Z52JListDemo mainFrame = new Z52JListDemo();
+	  
+	  mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
+      mainFrame.pack();
+      mainFrame.setLocationByPlatform(true);
+	  mainFrame.setVisible(true);
+	  
   }
 }
