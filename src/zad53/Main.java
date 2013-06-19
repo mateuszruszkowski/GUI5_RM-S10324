ced0142694f424bf5031def74d449e793238a87d
Michal Rogozinski <mr@mr-servers.net>
2013-06-19 00:42:34 -0700

    realizacja zadania Z53.

--- src/zad53/Main.java
+++ src/zad53/Main.java
@@ -1,13 +1,22 @@
 /**
  *
- *  @author Rogozi?ski Micha? S10324
+ *  @author Rogoziński Michał S10324
  *
  */
 package zad53;
+
+import javax.swing.JFrame;
 
 
 public class Main {
   public static void main(String ... args) {
   
+	  Z53JTableDemo mainFrame = new Z53JTableDemo();
+	  
+	  mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);      
+      mainFrame.pack();
+      mainFrame.setLocationByPlatform(true);
+	  mainFrame.setVisible(true);
+	  
   }
 }
